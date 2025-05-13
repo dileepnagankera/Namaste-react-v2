a875ecb9159b4d82b797f2a18597638a4d8233e9
dileepnagankera <dileepnagankera@gmail.com>
2025-05-13 14:11:45 +0530

    Episode 02

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-const parent= React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"I'm an h1 Tag"),
-        React.createElement("h1",{},"I'm an h2 Tag")
-    ]),
-     React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"I'm an h1 Tag"),
-        React.createElement("h1",{},"I'm an h2 Tag")
-    ])
-])
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { key: "h1" }, "I'm an h1 Tag"),
+    React.createElement("h2", { key: "h2" }, "I'm an h2 Tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { key: "h1" }, "I'm an h1 Tag"),
+    React.createElement("h2", { key: "h2" }, "I'm an h2 Tag"),
+  ]),
+]);
 
 console.log("parent", parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
